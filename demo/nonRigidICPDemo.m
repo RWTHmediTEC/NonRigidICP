8632cf6823de39bec0b588cef66b041628adb41c
Christoph Haenisch <haenisch@hia.rwth-aachen.de>
2014-12-11 13:16:55 +0100

    - Adapted the return values of nonRigidICP.m such that now a complete mesh is returned - Enhanced function indetation - Adapted nonRigidICPDemo.m accordingly and also extended it - Updated the reconstruction (STL) according to the result of the demo

--- demo/nonRigidICPDemo.m
+++ demo/nonRigidICPDemo.m
@@ -1,33 +1,65 @@
-%% Load exteral functions
-path_backup = path();
-addpath('..');
-addpath('../src');
+function nonRigidICPDemo()
+    %% Load external functions
 
-%% Load Template
-templateFilename = 'template.stl';
-[mesh_template.vertices, mesh_template.faces] = READ_stl(templateFilename);
-[mesh_template.vertices, mesh_template.faces] = patchslim(mesh_template.vertices, mesh_template.faces);
+    path_backup = path();
+    prefix = fileparts(mfilename('fullpath')); % path to current m-file
+    addpath([prefix '/..']);
+    addpath([prefix '/../src']);
 
-%% Load Target
-targetFilename = 'target.stl';
-[mesh_target.vertices, mesh_target.faces] = READ_stl(targetFilename);
-[mesh_target.vertices, mesh_target.faces] = patchslim(mesh_target.vertices, mesh_target.faces);
 
-%% Roughly align template and target (if not already done)
-% here you have to put in some form of manual alignment (matrix transformation)
-% or algorithm that roughly alignes the two datasets
-mesh_template.vertices = bsxfun(@minus, mesh_template.vertices, mean(mesh_template.vertices));
-mesh_target.vertices = bsxfun(@minus, mesh_target.vertices, mean(mesh_target.vertices));
+    %% Load Template
 
-%% Plot template and target (to test if they are roughly aligned)
-%myPlot(mesh_template.vertices, mesh_template.faces, mesh_target.vertices, mesh_target.faces, 0.5, 1.0);
+    templateFilename = 'template.stl';
+    [template_mesh.vertices, template_mesh.faces] = READ_stl(templateFilename);
+    [template_mesh.vertices, template_mesh.faces] = patchslim(template_mesh.vertices, template_mesh.faces);
 
-%% Reconstruction 
-[reconstruction_vertices, weights] = nonRigidICP(mesh_template, mesh_target);
 
-%% Write reconstruction to STL-file
-stlwrite('nonRigidICP_Reconstruction.stl', mesh_template.faces, reconstruction_vertices);
+    %% Load Target
 
-%% Redo changes to Path
-path(path_backup);
-    
+    targetFilename = 'target.stl';
+    [target_mesh.vertices, target_mesh.faces] = READ_stl(targetFilename);
+    [target_mesh.vertices, target_mesh.faces] = patchslim(target_mesh.vertices, target_mesh.faces);
+
+
+    %% Roughly align template and target (if not already done)
+
+    % Here you have to put in some form of manual alignment (matrix
+    % transformation) or algorithm that roughly alignes the two datasets.
+
+    template_mesh.vertices = bsxfun(@minus, template_mesh.vertices, mean(template_mesh.vertices));
+    target_mean = mean(target_mesh.vertices);
+    target_mesh.vertices = bsxfun(@minus, target_mesh.vertices, target_mean);
+
+    %% Plot template and target (to test if they are roughly aligned)
+
+    % myPlot(template_mesh.vertices, template_mesh.faces, target_mesh.vertices, target_mesh.faces, 0.5, 1.0);
+    %
+    % or
+    %
+    % plotPolygonMesh(template_mesh, 'Lighting', true)
+    % plotPolygonMesh(target_mesh)
+    % axis ('equal', 'off')
+
+
+    %% Reconstruction 
+
+    % Define some stiffness parameters if the default parameters do not fit your needs.
+    t = 0:15;
+    alpha = 1e8 * exp(-t');
+
+    [reconstruction, weights] = nonRigidICP(template_mesh, target_mesh, 'alpha', alpha);
+
+    % Shift the reconstruction in such a manner that it is aligned with the
+    % original target data set.
+    reconstruction.vertices = bsxfun(@plus, reconstruction.vertices, target_mean);
+
+
+    %% Write reconstruction to STL-file
+
+    stlwrite('nonRigidICP_Reconstruction.stl', template_mesh.faces, reconstruction.vertices);
+
+
+    %% Rervert path changes
+    path(path_backup);
+end
+
