d527ffff2ee7d4b7dfcaaa547b59e8c6aa4c585b
Christoph Hänisch <haenisch@hia.rwth-aachen.de>
2016-11-28 09:51:33 +0100

    Made this library compliant with the new library including scheme (LibraryImporter).

---
+++ install.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo Library installer script
+echo ------------------------
+echo[
+
+set LIBRARY_NAME=NonRigidICP
+set VERSION=1.3.0
+
+if "%MATLAB_LIBS%" == "" (
+    echo Error: The environment variable 'MATLAB_LIBS' is not set.
+    pause
+    exit -1
+) else (
+    echo Installing "%LIBRARY_NAME%-%VERSION%" in "%MATLAB_LIBS%".
+    echo Close this window to abort the installation.
+    timeout /t 15
+
+    if not exist "%MATLAB_LIBS%" (
+        echo Creating folder "%MATLAB_LIBS%"...
+        mkdir "%MATLAB_LIBS%"
+    )
+
+    if not exist "%MATLAB_LIBS%\%LIBRARY_NAME%-%VERSION%" (
+        echo Creating folder "%LIBRARY_NAME%-%VERSION%" in "%MATLAB_LIBS%"...
+        mkdir "%MATLAB_LIBS%\%LIBRARY_NAME%-%VERSION%"
+    )
+
+    echo Copying the data...
+    if exist exclude.txt rm exclude.txt
+    echo %~dp0.git >> exclude.txt
+    echo %~dp0src\codegen >> exclude.txt
+    echo exclude.txt >> exclude.txt
+    xcopy . "%MATLAB_LIBS%\%LIBRARY_NAME%-%VERSION%\" /f /y /e /EXCLUDE:exclude.txt
+    rm exclude.txt
+
+    pause
+)
