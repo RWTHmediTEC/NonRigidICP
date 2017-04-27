f47bba32379bf1f4063e4aa8f839b5d61145978f
Christoph Hänisch <haenisch@hia.rwth-aachen.de>
2017-04-27 13:23:03 +0200

    - Now, the axis limits remain constant. - Added the 'WindowSize' option. - Very few code clean up.

--- demo/makeVideoFromMorphing.m
+++ demo/makeVideoFromMorphing.m
@@ -15,22 +15,30 @@ function varargout = makeVideoFromMorphing(output_filename, source_mesh, target_
 %    Additional optional arguments can be passed in the form of pairs with
 %    the following meanings:
 %
-%       'FrameRate'                - Specifies the frame rate of the
-%                                    resulting video file. Must be a
-%                                    positive scalar. Defaults to 5.
+%       'FrameRate'             -  Specifies the frame rate of the
+%                                  resulting video file. Must be a positive
+%                                  scalar. Defaults to 5.
 %
-%       'SourceMeshOpacity'        - Opacity of the source mesh's faces
-%                                    when displayed. Must be a value
-%                                    between 0 and 1. Defaults to 1.
+%       'SourceMeshOpacity'     -  Opacity of the source mesh's faces when
+%                                  being displayed. Must be a value between
+%                                  0 and 1. The default value is 1.
 %
-%       'TargetMeshOpacity'        - Opacity of the target mesh's faces
-%                                    when displayed. Must be a value
-%                                    between 0 and 1. Defaults to 0.5.
+%       'TargetMeshOpacity'     -  Opacity of the target mesh's faces when
+%                                  being displayed. Must be a value between
+%                                  0 and 1. The default value is 0.5.
 %
-%       'View'                     - Viewing angles [az, el] for a
-%                                    three-dimensional plot. Defaults to
-%                                    a value of [10 30].
+%       'View'                  -  Viewing angles [az, el] for a three-
+%                                  dimensional plot. Defaults to [10 30].
 %
+%       'WindowSize'            -  Specifies the size of the output figure.
+%                                  Possible values are 'fullscreen'
+%                                  (default value) and 'default' where the
+%                                  latter means that the figure size is
+%                                  defined by Matlab. Alternatively, you
+%                                  can specify the figure position by
+%                                  passing a vector with normalized units
+%                                  of measurement like [0 0 0.5 1]. See
+%                                  'figure properties' for more details.
 %    Example
 %
 %       importer = LibraryImporter();
@@ -39,15 +47,13 @@ function varargout = makeVideoFromMorphing(output_filename, source_mesh, target_
 %       template = shiftMeshToOrigin(loadSTL('template.stl'));
 %       target = shiftMeshToOrigin(loadSTL('target.stl'));
 %       frames = makeVideoFromMorphing('test.avi', template, target, ...
-%                           'alpha', [1e9 1e7 1e5 1e3 100 10 1 0.1 0.01]');
-%       save frames.mat frames
+%                     'alpha', [1e9 1e7 1e5 1e3 100 10 1 0.1 0.01 0.001]');
 %       figure;
 %       movie(frames, 1, 2)
-%       exit
 
 % Copyright 2015, 2017 Chair of Medical Engineering, RWTH Aachen University
 % Written by Christoph Hänisch (haenisch@hia.rwth-aachen.de)
-% Version 1.1
+% Version 1.2
 % Last changed on 2017-04-27.
 % License: Modified BSD License (BSD license with non-military-use clause)
 
@@ -66,7 +72,7 @@ function varargout = makeVideoFromMorphing(output_filename, source_mesh, target_
         error('Parameter list must have an even length.')
     end
 
-    known_keywords = {'FrameRate', 'SourceMeshOpacity', 'TargetMeshOpacity', 'View'};
+    known_keywords = {'FrameRate', 'SourceMeshOpacity', 'TargetMeshOpacity', 'View', 'WindowSize'};
 
     input_args = {};
     unknown_args = {};
@@ -85,12 +91,25 @@ function varargout = makeVideoFromMorphing(output_filename, source_mesh, target_
     addParameter(parser, 'SourceMeshOpacity', 1.0, @(x)validateattributes(x,{'numeric'},{'>=',0},{'<=',1}, 'makeVideoFromMorphing'));
     addParameter(parser, 'TargetMeshOpacity', 0.5, @(x)validateattributes(x,{'numeric'},{'>=',0},{'<=',1}, 'makeVideoFromMorphing'));
     addParameter(parser, 'View', [10 30]);
+    addParameter(parser, 'WindowSize', 'fullscreen', @(x) isvector(x) && length(x) == 4 || any(validatestring(x, {'default', 'fullscreen'})));
     parse(parser, input_args{:});
+
+    %% Create a new window
+
+    fig = figure;
+
+    window_size = parser.Results.WindowSize;
+    if strcmpi(window_size, 'default')
+        % do nothing
+    elseif strcmpi(window_size, 'fullscreen')
+        fullscreen
+    elseif isvector(window_size)
+        fig.Units = 'normalized';
+        fig.Position = window_size;
+    end
 
     %% Perform the morphing and save store the obtained geometries
 
-    figure
-    hold on
     patch_handle_target_mesh = plotPolygonMesh(target_mesh, 'Lighting', true, 'ColorScheme', 'Transparent');
     patch_handle_target_mesh.FaceAlpha = parser.Results.TargetMeshOpacity;
     patch_handle_source_mesh = plotPolygonMesh(source_mesh, 'Color', 'g', 'ColorScheme', 'Transparent');
@@ -98,8 +117,11 @@ function varargout = makeVideoFromMorphing(output_filename, source_mesh, target_
     axis off equal
     view(parser.Results.View)
     legend({'Target mesh', 'Source mesh'})
-    fullscreen
     drawnow
+    ax = gca();
+    x_limits = xlim(ax);
+    y_limits = ylim(ax);
+    z_limits = zlim(ax);
     frames(1) = getframe(gcf);
 
     nonRigidICP(source_mesh, target_mesh, unknown_args{:}, 'callback', @callback);
@@ -142,6 +164,9 @@ function varargout = makeVideoFromMorphing(output_filename, source_mesh, target_
         patch_handle_source_mesh.FaceAlpha = parser.Results.SourceMeshOpacity;
         title({sprintf('Iteration %d, inner iteration %d', data.iteration, data.inner_iteration), ...
                sprintf('alpha %.4f, diff %.4f, inliers %d', data.alpha, data.gradual_change, data.number_of_inliers)})
+        xlim(ax, x_limits)
+        ylim(ax, y_limits)
+        zlim(ax, z_limits)
         drawnow
         frames(end+1) = getframe(gcf);
     end
